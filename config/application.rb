249275bcffdb4e08b5ee699a1020bc4806d89e00
Zach Nusbaum <zachdnusbaum@gmail.com>
2017-09-16 22:58:58 -0700

    timezone = phoenix

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module ZdnSelfService
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = "America/Phoenix"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
